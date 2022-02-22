6d5e977378a94e59efcdeadd7d76c33dae166277
crarun911 <crarun911@gmail.com>
2022-02-22 17:12:29 +0530

    last commit with typeconverter room

---
+++ app/src/main/java/com/example/employeedirectoryapplication/EmployeeApp.kt
@@ -0,0 +1,8 @@
+package com.example.employeedirectoryapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EmployeeApp:Application() {
+}
